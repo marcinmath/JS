ddf69563a4f273d1962d81a704a93e4da1073c22
Marcin Zalewski <Marcin.Zalewski@acxiom.com>
2017-09-17 22:56:17 +0200

    Changes to mocha test.

--- tests/SeleniumMocha.js
+++ tests/SeleniumMocha.js
@@ -1,11 +1,25 @@
 var assert = require('assert'),
     test = require('selenium-webdriver/testing'),
-    webdriver = require('selenium-webdriver');
+    webdriver = require('selenium-webdriver'),
+    username = "mzalew",
+    accessKey = "6af6f5db-c2be-4271-885f-23a9e013fc54";
 
 test.describe('Google Search', function() {
     test.it('should work', function() {
+        // var driver = new webdriver.Builder().
+        // withCapabilities(webdriver.Capabilities.chrome()).
+        // build();
         var driver = new webdriver.Builder().
-        withCapabilities(webdriver.Capabilities.chrome()).
+        withCapabilities({
+            'browserName': 'chrome',
+            'platform': 'Linux',
+            'version': '43.0',
+            'username': username,
+            'accessKey': accessKey,
+            'name': ' Sample selenium test'
+        }).
+        usingServer("http://" + username + ":" + accessKey +
+            "@ondemand.saucelabs.com:80/wd/hub").
         build();
         driver.get('http://www.google.com');
         var searchBox = driver.findElement(webdriver.By.name('q'));
